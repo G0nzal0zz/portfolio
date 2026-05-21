1438071ea1b8aeefa13f140d3a1819a5e813af47
gonzalo <larroyagonzalo+git@gmail.com>
2026-05-21 20:57:38 +0100

    fix error during build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { resolve } from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
@@ -12,4 +11,9 @@ export default defineConfig({
     tailwindcss(),
   ],
   base: '/vite-deploy-demo/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
